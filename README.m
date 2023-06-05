a82a97d090d575fb4a0a6fc86617435910367139
Manish Agrahari <manish.agrahari2015@gmail.com>
2023-06-05 13:37:20 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,5 +1,5 @@
 # StriverSdeSheetChallenge
 #Day1
-- **Set Matrix Zeros**  (https://www.codingninjas.com/codestudio/problems/set-matrix-zeros_8230862?challengeSlug=striver-sde-challenge&leftPanelTab=0)
+- **SetMatrixZeros**
 
 
